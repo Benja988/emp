5c02f7eefaa66af2b3688e987cbd2a0ac0bd3443
Benjamin Okumu <okumub85@gmail.com>
2025-02-26 12:20:06 +0300

    update

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'EMP'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'enjoy more'
+export const APP_DESCRIPTION =  process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'An ecommerce website for making your work easier'
